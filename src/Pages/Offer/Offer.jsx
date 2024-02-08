c12f0e3a8807e884a87ea716f471fe15cd6b0a73
MuhammadAtiq757 <muhammadatiq757@gmail.com>
2024-02-08 14:20:52 +0600

    done

--- src/Pages/Offer/Offer.jsx
+++ src/Pages/Offer/Offer.jsx
@@ -51,22 +51,23 @@ const Offer = () => {
                 </div>
 
 
-                <div className='  grid md:grid-cols-3 gap-2 mx-auto px-10 pr-4  pt-24'>
-                    <div className='card justify-center items-center  p-4 mr-2 rounded-lg broder border-1 hover:text-white  bg-gray-100 shadow-lg border-y-2 border-blue-400 '>
+                <div className='  grid md:grid-cols-3 gap-2 mx-auto px-6 items-center justify-center  text-center  pt-24'>
+                   
+                    <div className='card justify-center items-center  p-3  rounded-lg broder border-1  bg-gray-100 shadow-lg border-y-2 border-blue-400 '>
                         <MdOutlineChromeReaderMode className='mx-auto text-5xl  mb-4 text-blue-600' />
                         <h1 className='text-1xl font-serif font-semibold text-black'>Learn</h1>
-                        <p className='font-extralight text-gray-600 pt-2 '>Lorem, ipsum dolor sit amet  consectetur  adipisicing elit.Voluptatem, quibusdam.</p>
+                        <p className='font-extralight text-gray-600 pt-4'>Lorem, ipsum dolor sit amet  consectetur  adipisicing elit.Voluptatem, quibusdam.</p>
                     </div>
 
 
-                    <div className='card justify-center items-center  p-4 mr-2 rounded-lg broder border-1  bg-gray-100 shadow-lg border-y-2 border-blue-400 '>
+                    <div className='card justify-center items-center  p-4  rounded-lg broder border-1  bg-gray-100 shadow-lg border-y-2 border-blue-400 '>
                         <FaPenToSquare className='mx-auto text-5xl mb-4  text-blue-600' />
                         <h1 className='text-1xl font-serif font-semibold text-black'>Practice</h1>
                         <p className='font-extralight text-gray-600 pt-2'>Lorem, ipsum dolor sit amet  consectetur  adipisicing elit. Voluptatem, quibusdam.</p>
                     </div>
 
 
-                    <div className='card justify-center items-center  p-4 mr-2 hover:text-white rounded-lg broder border-1  bg-gray-100 shadow-lg border-y-2 border-blue-400 '>
+                    <div className='card justify-center items-center  p-4  hover:text-white rounded-lg broder border-1  bg-gray-100 shadow-lg border-y-2 border-blue-400 '>
                         <PiPlantBold className='mx-auto text-5xl mb-4  text-blue-600' />
                         <h1 className='text-1xl font-serif font-semibold text-black'>Grow</h1>
                         <p className='font-extralight text-gray-600 pt-2 '>Lorem, ipsum dolor sit amet  consectetur  adipisicing elit. Voluptatem, quibusdam.</p>
